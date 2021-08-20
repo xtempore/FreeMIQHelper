a82627d6798547ba6541d80b57d6e1d0b15e5909
Jon <jbriden@gmail.com>
2021-08-20 20:27:00 +1200

    Version 3.1.1 - Minor change to make available dates more visible

--- MIQ Search.js
+++ MIQ Search.js
@@ -166,6 +166,7 @@ unsafeWindow.quit = false;
             dt.setFullYear(dt.getFullYear() + 1);
           }
           availDates.set(dateFmt(dt, dateFKey), label);
+          label.style.background = "yellow";
         }
       }
     }
